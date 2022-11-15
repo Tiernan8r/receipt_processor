b9971238366d8ceb4d436c05e5a5c22442d6fb15
Tiernan8r <tiernan8r@pm.me>
2022-11-15 17:01:51 +0000

    pep8s

--- src/ocr/__init__.py
+++ src/ocr/__init__.py
@@ -12,4 +12,4 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import src.ocr.extract as extract
+import src.ocr.extract as extract  # noqa: F401
