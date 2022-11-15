93ba7539583a11a44aba25937a1edac7c0c0e29a
Tiernan8r <tiernan8r@pm.me>
2022-11-15 15:50:08 +0000

    separate scan.py steps into methods

--- src/scan.py
+++ src/scan.py
@@ -1,89 +1,116 @@
 # Based on code originally from
 # https://pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/
 
-# import the necessary packages
-import argparse
 import sys
 
 import cv2
 import imutils
-import numpy as np
-# from skimage.filters import threshold_local
 
 from transform import four_point_transform
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="Path to the image to be scanned")
-args = vars(ap.parse_args())
+# from skimage.filters import threshold_local
 
-# load the image and compute the ratio of the old height
-# to the new height, clone it, and resize it
-image = cv2.imread(args["image"])
-ratio = image.shape[0] / 500.0
-orig = image.copy()
-image = imutils.resize(image, height=500)
-# convert the image to grayscale, blur it, and find edges
-# in the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(gray, 75, 200)
-# show the original image and the edge detected image
-print("STEP 1: Edge Detection")
-# cv2.imshow("Image", image)
-img_stat = cv2.imwrite("./out/image.JPEG", image)
-print("input image written successfully:", img_stat)
-# cv2.imshow("Edged", edged)
-img_stat = cv2.imwrite("./out/edged.JPEG", edged)
-print("edged image written successfully:", img_stat)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
-# find the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
-cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
 
-screenCnt = None
-# loop over the contours
-for c in cnts:
-    # approximate the contour
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-    print("APPROX:", approx, "L=", len(approx))
-    # if our approximated contour has four points, then we
-    # can assume that we have found our screen
-    if len(approx) == 4:
-        screenCnt = approx
-        break
+def scan(path):
+    image = read_image(path)
+    image, orig, ratio = reshape(image)
+    edged = detect_edges(image)
+    screen_contours = find_contours(edged)
+    warped = perspective_transform(image, screen_contours, orig, ratio)
 
-if screenCnt is None:
-    print("Contours not found!")
-    sys.exit(1)
+    show_result(orig, warped)
 
-# show the contour (outline) of the piece of paper
-print("STEP 2: Find contours of paper")
-cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-# cv2.imshow("Outline", image)
-cv2.imwrite("out/outline.JPEG", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
-# apply the four point transform to obtain a top-down
-# view of the original image
-warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
-# convert the warped image to grayscale, then threshold it
-# to give it that 'black and white' paper effect
-warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-# T = threshold_local(warped, 11, offset=10, method="gaussian")
-# warped = (warped > T).astype("uint8") * 255
+def read_image(path):
+    image = cv2.imread(path)
 
-# show the original and scanned images
-# print("STEP 3: Apply perspective transform")
-# cv2.imshow("Original", imutils.resize(orig, height = 650))
-cv2.imwrite("out/original.JPEG", imutils.resize(orig, height=650))
-# cv2.imshow("Scanned", imutils.resize(warped, height = 650))
-cv2.imwrite("out/scanned.JPEG", imutils.resize(warped, height=650))
-# cv2.waitKey(0)
+    return image
+
+
+def reshape(image):
+    # compute the ratio of the old height
+    # to the new height, clone it, and resize it
+    ratio = image.shape[0] / 500.0
+    orig = image.copy()
+    image = imutils.resize(image, height=500)
+
+    return image, orig, ratio
+
+
+def detect_edges(image):
+    # convert the image to greyscale, blur it, and find edges
+    # in the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+    # show the original image and the edge detected image
+    print("STEP 1: Edge Detection")
+    # cv2.imshow("Image", image)
+    img_stat = cv2.imwrite("./out/image.JPEG", image)
+    print("input image written successfully:", img_stat)
+    # cv2.imshow("Edged", edged)
+    img_stat = cv2.imwrite("./out/edged.JPEG", edged)
+    print("edged image written successfully:", img_stat)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    return edged
+
+
+def find_contours(edged):
+    # find the contours in the edged image, keeping only the
+    # largest ones, and initialize the screen contour
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+    screenCnt = None
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points, then we
+        # can assume that we have found our screen
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+
+    if screenCnt is None:
+        print("Contours not found!")
+        sys.exit(1)
+
+    return screenCnt
+
+
+def perspective_transform(image, screenCnt, orig, ratio):
+    # show the contour (outline) of the piece of paper
+    print("STEP 2: Find contours of paper")
+    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+    # cv2.imshow("Outline", image)
+    cv2.imwrite("out/outline.JPEG", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # apply the four point transform to obtain a top-down
+    # view of the original image
+    warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+    # convert the warped image to greyscale, then threshold it
+    # to give it that 'black and white' paper effect
+    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+    # T = threshold_local(warped, 11, offset=10, method="gaussian")
+    # warped = (warped > T).astype("uint8") * 255
+
+    return warped
+
+
+def show_result(orig, warped):
+    # show the original and scanned images
+    print("STEP 3: Apply perspective transform")
+    # cv2.imshow("Original", imutils.resize(orig, height = 650))
+    cv2.imwrite("out/original.JPEG", imutils.resize(orig, height=650))
+    # cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+    cv2.imwrite("out/scanned.JPEG", imutils.resize(warped, height=650))
+    # cv2.waitKey(0)
